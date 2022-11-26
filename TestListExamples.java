9671a57888881b2e29cd6b1688cf540b5551a9b2
tlacaba <timlacaba123@gmail.com>
2022-11-25 17:11:34 -0800

    added tests, fixed some access issues

--- TestListExamples.java
+++ TestListExamples.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 import org.junit.*;
 
-public class StartsWithCapital implements StringChecker {
+class StartsWithCapital implements StringChecker {
   public boolean checkString(String s) {
     if (s.length() > 0) return Character.isUpperCase(s.charAt(0));
     return false;
@@ -17,8 +17,8 @@ public class StartsWithCapital implements StringChecker {
 
 public class TestListExamples {
   // Write your grading tests here!
-  @Test
-  public void testFilter() {
+  @Test(timeout = 2000)
+  public void testFilter1() {
     List<String> input1 = new ArrayList<String>();
     input1.add("The");
     input1.add("Quick");
@@ -39,7 +39,14 @@ public class TestListExamples {
     assertEquals(expected, ListExamples.filter(input1, new StartsWithCapital()));
   }
 
-  @Test
+  @Test(timeout = 2000)
+  public void testFilter2() {
+    List<String> input1 = new ArrayList<String>();
+    List<String> expected = new ArrayList<String>();
+    assertEquals(expected, ListExamples.filter(input1, new StartsWithCapital()));
+  }
+
+  @Test(timeout = 2000)
   public void testMerge1() {
     List<String> input1 = new ArrayList<>();
     input1.add("apple");
@@ -65,7 +72,7 @@ public class TestListExamples {
     assertEquals(expected, ListExamples.merge(input1, input2));
   }
 
-  @Test
+  @Test(timeout = 2000)
   public void testMerge2() {
     List<String> input1 = new ArrayList<>();
     List<String> input2 = new ArrayList<>();
